b5487a765ff38422ff29bcdfd0636cfea8ec2975
Arsh Ergon <arshergon@gmail.com>
2020-05-01 18:06:14 +0530

    done

--- ads/urls.py
+++ ads/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "ads"
 
 urlpatterns = [
-    path('', views.desktop_home_page, name="desktop_home_page"),
+    path('desktop/', views.desktop_home_page, name="desktop_home_page"),
     path('<slug:slug_title>/', views.desktop_single_page_recipes, name='desktop_single_page_recipes'),
+    path('', views.mobile_home_page, name="mobile_home"),
 ]
--- ads/views.py
+++ ads/views.py
@@ -16,3 +16,7 @@ def desktop_single_page_recipes(request, slug_title):
     single_recipe = get_object_or_404(RecipeModel, slug_title=slug_title)
     recom_recipes = RecipeModel.objects.order_by("?")[:3]
     return render(request, 'desktop/single/single_page.html', {'seo_data':seo_loop, 'single_rec_desplay':single_recipe, "recom_recipes":recom_recipes})
+
+
+def mobile_home_page(request):
+    return render(request, "mobile/mobile_index.html")
